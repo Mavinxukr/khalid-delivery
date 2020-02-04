065f8bb082039308720be9d1c8c5c8bfaacc24ce
Mavinxukr <mavinx.office@gmail.com>
2020-02-04 13:09:32 +0000

    first

--- app/Http/Controllers/ApiClient/Auth/AuthController.php
+++ app/Http/Controllers/ApiClient/Auth/AuthController.php
@@ -125,14 +125,14 @@ class AuthController extends Controller
 
     public function apple()
     {
-        return Socialite::driver("apple")
+        return Socialite::driver("sign-in-with-apple")
             ->redirect();
     }
 
 
     public function appleCallback()
     {
-        $user = Socialite::driver("apple")
+        $user = Socialite::driver("sign-in-with-apple")
             ->user();
         dd($user);
     }
