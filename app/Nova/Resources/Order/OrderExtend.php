952aad6ef2962ce39e1745f95a64313e005e77f5
Mavinx <mavinx.office@gmail.com>
2020-08-26 14:38:46 +0300

    Add SP Extends to Admin Panel

---
+++ app/Nova/Resources/Order/OrderExtend.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Nova\Resources\Order;
+
+use App\Nova\Resource;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\HasMany;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+
+class OrderExtend extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'App\Models\Order\OrderExtend';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    public static function label()
+    {
+        return 'Orders Extends';
+    }
+
+
+    public static $category = "Order";
+
+
+    public static $group = 'Order';
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->sortable(),
+            BelongsTo::make('Order', 'order'),
+            Text::make('Extend From', 'extend_from'),
+            Text::make('Extend To', 'extend_to'),
+            Text::make('Reason', 'reason'),
+            Text::make('Initial Cost', 'initial_cost'),
+            Text::make('Cost', 'cost'),
+            Text::make('Service Received', 'service_received'),
+            Text::make('Company Received', 'company_received'),
+            Boolean::make('Accepted', 'accepted'),
+            HasMany::make('Files', 'files', OrderExtendFile::class)
+                ->onlyOnDetail(),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,6 +7,8 @@ use App\Models\Invoice\InvoiceTemplate;
 use App\Models\Message\SmsToUser;
 use App\Models\Order\Order;
 use App\Models\Order\OrderDetail;
+use App\Models\Order\OrderExtend;
+use App\Models\Order\OrderExtendFile;
 use App\Models\Order\PreOrder;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -26,6 +28,8 @@ class AuthServiceProvider extends ServiceProvider
         PreOrder::class => 'App\Policies\PreOrderPolicy',
         OrderDetail::class => 'App\Policies\OrderDetailPolicy',
         InvoiceTemplate::class => 'App\Policies\InvoiceTemplatePolicy',
+        OrderExtend::class => 'App\Policies\OrderExtendPolicy',
+        OrderExtendFile::class => 'App\Policies\OrderExtendFilePolicy',
     ];
 
     /**
