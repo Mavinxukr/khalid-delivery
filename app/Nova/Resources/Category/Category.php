5d265eed81b5410ad504d43529e2eddde29565ca
Mavinxukr <mavinx.office@gmail.com>
2020-08-26 14:42:33 +0300

    sdd

--- app/Nova/Resources/Category/Category.php
+++ app/Nova/Resources/Category/Category.php
@@ -59,7 +59,11 @@ class Category extends Resource
             Text::make('Type')
                 ->sortable()
                 ->rules('required', 'max:255'),
-            Text::make('Fee')->nullable(),
+            Text::make('Fee')
+                ->displayUsing(function ($i){
+                    return $i.'%';
+                })
+                ->nullable(),
             Boolean::make('Active')
                 ->trueValue(1)
                 ->falseValue(0),
